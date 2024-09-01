5e69f35d0c173647c81db027bfb943c155913468
一起养条鱼吧 <2966994290@qq.com>
2024-09-01 10:24:32 +0800

    添加项目文件。

---
+++ JenkinsTest/Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JenkinsTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet(Name = "Get")]
+        public IActionResult Get()
+        {
+            return Ok("V1");
+        }
+    }
+}
